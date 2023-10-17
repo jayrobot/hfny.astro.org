c22901513629fea30c26505e497fc74ccbf4b18a
Jay Robohn <jayrobot@gmail.com>
2023-10-17 11:02:20 -0400

    Initial commit

---
+++ studio/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'hfny-astro',
+
+  projectId: 'z6z32j5s',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
